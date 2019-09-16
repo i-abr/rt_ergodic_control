86d5c6ac3cfd38b1297546f1717cc9fac456695c
Ian Abraham <iabr4073@gmail.com>
2019-09-16 17:26:13 -0500

    moved the files into the lib folder; created an agent class; incorporated ros

--- main.py
+++ main.py
@@ -1,50 +1,62 @@
-from double_integrator import DoubleIntegrator
-from ergodic_control import RTErgodicControl
-from target_dist import TargetDist
-from utils import convert_phi2phik, convert_ck2dist, convert_traj2ck, convert_phik2phi
-import autograd.numpy as np
+#!/usr/bin/env python
 
-import matplotlib.pyplot as plt
+from rt_erg_lib.agent import Agent
 
 import os
+import argparse
+
+import rospy
+
+parser = argparse.ArgumentParser()
+parser.add_argument('agent_num', type=int, help='agent number', default=0)
+args = parser.parse_args()
+
 
 if __name__ == '__main__':
-    env         = DoubleIntegrator()
-    model       = DoubleIntegrator()
-    t_dist      = TargetDist(num_nodes=2)
-    erg_ctrl    = RTErgodicControl(model, t_dist,
-                            horizon=15, num_basis=5, batch_size=-1)
 
-    # setting the phik on the ergodic controller
-    erg_ctrl.phik = convert_phi2phik(erg_ctrl.basis, t_dist.grid_vals, t_dist.grid)
-
-    print('--- simulating ergodic coverage ---')
-    log = {'trajectory' : []}
-    tf = 200
-    state = env.reset()
-    for t in range(tf):
-        ctrl = erg_ctrl(state)
-        state = env.step(ctrl)
-        log['trajectory'].append(state)
-    print('--- finished simulating ---')
-
-    xy, vals = t_dist.get_grid_spec()
-    plt.figure(1)
-    plt.title('ergodic coverage')
-    plt.contourf(*xy, vals, levels=10)
-    xt = np.stack(log['trajectory'])
-    plt.scatter(xt[:tf,0], xt[:tf,1])
-
-    path = xt[:tf,model.explr_idx]
-    ck = convert_traj2ck(erg_ctrl.basis, path)
-    val = convert_ck2dist(erg_ctrl.basis, ck)
-
-    plt.figure(2)
-    plt.title('time averaged statistics')
-    plt.contourf(*xy, val.reshape(50,50), levels=10)
-    plt.figure(3)
-    plt.title('Fourier reconstruction of target distribution')
-    phi = convert_phik2phi(erg_ctrl.basis, erg_ctrl.phik, t_dist.grid)
-    plt.contourf(*xy, phi.reshape(50,50), levels=10)
-
-    plt.show()
+    agent = Agent(args.agent_num)
+    
+    try:
+        agent.run()
+    except rospy.ROSInterruptException:
+        pass
+    
+#    env         = DoubleIntegrator()
+#    model       = DoubleIntegrator()
+#    t_dist      = TargetDist(num_nodes=2)
+#    erg_ctrl    = RTErgodicControl(model, t_dist,
+#                            horizon=15, num_basis=5, batch_size=-1)
+#
+#    # setting the phik on the ergodic controller
+#    erg_ctrl.phik = convert_phi2phik(erg_ctrl.basis, t_dist.grid_vals, t_dist.grid)
+#
+#    print('--- simulating ergodic coverage ---')
+#    log = {'trajectory' : []}
+#    tf = 200
+#    state = env.reset()
+#    for t in range(tf):
+#        ctrl = erg_ctrl(state)
+#        state = env.step(ctrl)
+#        log['trajectory'].append(state)
+#    print('--- finished simulating ---')
+#
+#    xy, vals = t_dist.get_grid_spec()
+#    plt.figure(1)
+#    plt.title('ergodic coverage')
+#    plt.contourf(*xy, vals, levels=10)
+#    xt = np.stack(log['trajectory'])
+#    plt.scatter(xt[:tf,0], xt[:tf,1])
+#
+#    path = xt[:tf,model.explr_idx]
+#    ck = convert_traj2ck(erg_ctrl.basis, path)
+#    val = convert_ck2dist(erg_ctrl.basis, ck)
+#
+#    plt.figure(2)
+#    plt.title('time averaged statistics')
+#    plt.contourf(*xy, val.reshape(50,50), levels=10)
+#    plt.figure(3)
+#    plt.title('Fourier reconstruction of target distribution')
+#    phi = convert_phik2phi(erg_ctrl.basis, erg_ctrl.phik, t_dist.grid)
+#    plt.contourf(*xy, phi.reshape(50,50), levels=10)
+#
+#    plt.show()
---
+++ rt_erg_lib/agent.py
@@ -0,0 +1,52 @@
+from double_integrator import DoubleIntegrator
+from ergodic_control import RTErgodicControl
+from target_dist import TargetDist
+from utils import convert_phi2phik, convert_ck2dist, convert_traj2ck, convert_phik2phi
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import os
+
+import rospy
+import tf
+
+class Agent(DoubleIntegrator):
+    
+    def __init__(self, agent_num=0):
+
+        DoubleIntegrator.__init__(self)
+
+        rospy.init_node('agent{}'.format(agent_num))
+        self.rate = rospy.Rate(30)
+
+        self.agent_name = 'agent{}'.format(agent_num)
+        self.model       = DoubleIntegrator()
+        self.t_dist      = TargetDist(num_nodes=2) #TODO: remove example target distribution
+        self.controller  = RTErgodicControl(self.model, self.t_dist,
+                                horizon=15, num_basis=5, batch_size=-1)
+
+        # setting the phik on the ergodic controller
+        self.controller.phik = convert_phi2phik(self.controller.basis, 
+                                                self.t_dist.grid_vals, 
+                                                self.t_dist.grid)
+        self.reset() # reset the agent 
+
+        self.broadcast = tf.TransformBroadcaster()
+        self.broadcast.sendTransform((self.state[0], self.state[1], 1.0),
+                                     (0, 0, 0, 1), # quaternion 
+                                     rospy.Time.now(),
+                                     self.agent_name,
+                                     "world")
+
+    def run(self):
+        while not rospy.is_shutdown():
+            ctrl = self.controller(self.state)
+            state = self.step(ctrl)
+            
+            self.broadcast.sendTransform((self.state[0], self.state[1], 1.0),
+                                         (0, 0, 0, 1), # quaternion 
+                                         rospy.Time.now(),
+                                         self.agent_name,
+                                         "world")
+            self.rate.sleep()
